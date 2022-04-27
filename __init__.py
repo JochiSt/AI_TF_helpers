87406fd92f9cebafebbaa28cd74463b423ddce08
JochiSt <jochen.steinmann@web.de>
2022-04-27 15:01:25 +0200

    fixed import paths

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-from get_flops import get_flops
-from storage import save_model
-from storage import save_model_ONNX
-from storage import save_model_image
+from .get_flops import get_flops
+from .storage import save_model
+from .storage import save_model_ONNX
+from .storage import save_model_image
