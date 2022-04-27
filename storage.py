27b12423d0edd68d5a6cfebdb6be3c581a2d6422
JochiSt <jochen.steinmann@web.de>
2022-04-27 14:09:03 +0200

    moved function into own file

--- storage.py
+++ storage.py
@@ -1,28 +1,7 @@
-
 # TensorFlow is an open source machine learning library
 import tensorflow as tf
 # Keras is TensorFlow's high-level API for deep learning
 from tensorflow import keras
-
-def getFLOPS(model):
-    """
-        get FLOPS of the model
-
-        2 FLOPS is about one MACC
-    """
-    # from https://github.com/tensorflow/tensorflow/issues/32809#issuecomment-768977280
-    from tensorflow.python.framework.convert_to_constants import  convert_variables_to_constants_v2_as_graph
-
-    concrete = tf.function(lambda inputs: model(inputs))
-    concrete_func = concrete.get_concrete_function(
-        [tf.TensorSpec([1, *inputs.shape[1:]]) for inputs in model.inputs])
-    frozen_func, graph_def = convert_variables_to_constants_v2_as_graph(concrete_func)
-    with tf.Graph().as_default() as graph:
-        tf.graph_util.import_graph_def(graph_def, name='')
-        run_meta = tf.compat.v1.RunMetadata()
-        opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
-        flops = tf.compat.v1.profiler.profile(graph=graph, run_meta=run_meta, cmd="op", options=opts)
-        return flops.total_float_ops
 
 def save_model(model, name=None, folder="storedANN"):
     """
