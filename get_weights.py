98887b84b9e25676be2db9a04a8a85e40464c9a6
JochiSt <jochen.steinmann@web.de>
2023-04-13 12:22:04 +0200

    changed to logarithmic binning for plotting weights added some labels to the axes

--- get_weights.py
+++ get_weights.py
@@ -15,6 +15,10 @@ def plot_weights( models):
     plt.figure( figsize=(7,7) )
     plt.semilogy()
 
+    bins = -1* np.flip( np.logspace(-3,1, num=250) )
+    bins = np.append(bins, np.linspace(-1e-3, 1e-3, 10))
+    bins = np.append(bins, np.logspace(-3,1, num=250))
+
     for i,model in enumerate(models):
         flat_weights = np.array( [] )
 
@@ -25,12 +29,18 @@ def plot_weights( models):
         # percentate of zeros in the weights
         rel_zeros = np.sum(flat_weights==0) / np.size(flat_weights)
 
-        h, b = np.histogram(flat_weights, bins=100)
-        plt.bar( b[:-1], h, width=b[1]-b[0], label="%s %5.3f %% zeros"%(model.name, rel_zeros*100) )
+        #h, b = np.histogram(flat_weights, bins=1000)
+        #plt.bar( b[:-1], h, width=b[1]-b[0], label="%s %5.3f %% zeros"%(model.name, rel_zeros*100) )
+
+        plt.hist( flat_weights, bins=bins, label="%s %5.3f %% zeros"%(model.name, rel_zeros*100) )
 
     model_names = ""
     for model in models:
         model_names += "_" + model.name
+
+    plt.xscale('symlog')
+    plt.xlabel('weights')
+    plt.ylabel('counts')
 
     plt.legend()
     plt.tight_layout()
